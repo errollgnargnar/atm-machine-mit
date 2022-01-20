f5904f7643b13ca09718486a3809abcd016abb1d
errollgnargnar <errollgnargnar@gmail.com>
2022-01-19 19:38:15 -0600

    doesnt allow negative withdrawals

--- account1.jsx
+++ account1.jsx
@@ -1,8 +1,10 @@
 // this keeps a running total of deposits and withdrawals
 
-const ATMDeposit = ({ onChange }) => {
+const ATMDeposit = ({ onChange, isDeposit }) => {
+  const choice = ['Deposit', 'Cash Back'];
   return (
     <label className="label huge">
+      <h3>{choice[Number(!isDeposit)]}</h3>
       Deposit:
       <input type="number" onChange={onChange}></input>
       <input type="submit"></input>
@@ -12,22 +14,36 @@ const ATMDeposit = ({ onChange }) => {
 
 const Account = () => {
   const [accountState, setAccountState] = React.useState(0);
-  var deposit = 0;
+  const [transactionState, setTransactionState] = React.useState(0);
+  const [isDeposit, setIsDeposit] = React.useState(true);
+
   const handleChange = event => {
     console.log(`handleChange ${event.target.value}`);
-    deposit = Number(event.target.value);
+    setTransactionState(event.target.value);
   };
   const handleSubmit = event => {
-    let newTotal = Number(accountState) + deposit;
-    alert(`Account total = ${newTotal}`);
-    setAccountState(newTotal);
-    event.preventDefault();
+        event.preventDefault();
+
+    if(!isDeposit && Number(transactionState) > Number(accountState)) alert('not enough funds')
+    else {
+      let newTotal = isDeposit ? Number(accountState) + Number(transactionState) : Number(accountState) - Number(transactionState);
+      setAccountState(newTotal);
+    }
+
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Account Balance {accountState}</h2>
-      <ATMDeposit onChange={handleChange}> Deposit</ATMDeposit>
+      <button onClick={(e) => {
+        e.preventDefault();
+        setIsDeposit(true);
+      }}>Deposit</button>
+      <button onClick={(e) => {
+        e.preventDefault();
+        setIsDeposit(false);
+      }}>Cash Back</button>
+      <ATMDeposit onChange={handleChange} isDeposit={isDeposit}> Deposit</ATMDeposit>
     </form>
   );
 };
